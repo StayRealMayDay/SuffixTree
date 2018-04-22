28b34aca360eb995d955f5ffa4e2ed99b38e4f38
renhaoran <RHRxjtu@outlook.com>
2018-04-22 18:31:19 +0800

    add some note about the ege and Node

--- Edge.cs
+++ Edge.cs
@@ -4,24 +4,40 @@ namespace SuffixTree
 {
     public class Edge
     {
+        /// <summary>
+        /// indicate the start location of this edge
+        /// </summary>
         public int From { set; get; }
-
+        /// <summary>
+        /// indicate the end index of this edge
+        /// </summary>
         public int To { get; set; }
-
+        /// <summary>
+        /// the length of this edge , also indicate number of item in this edge
+        /// </summary>
         public int Length { get; set; }
-
+        /// <summary>
+        /// init this edge
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
         public Edge(int from, int to)
         {
             this.From = from;
             this.To = to;
             this.Length = to - from;
         }
-
+        /// <summary>
+        /// use this method to return the newest length of this edge
+        /// </summary>
+        /// <returns></returns>
         public int GetLength()
         {
             return this.To - this.From > 0 ? this.To - this.From : int.MaxValue;
         }
-
+        /// <summary>
+        /// point to the node that this edge link to
+        /// </summary>
         public Node Next { get; set; }
     }
 }
--- Node.cs
+++ Node.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 namespace SuffixTree
 {
     public class Node
-    {
+    {   
+        /// <summary>
+        /// use a dictionart to store the all the edges that this node has
+        /// the key is the first item in the edge
+        /// the value is the edge which correspond to the key
+        /// </summary>
         public Dictionary<char, Edge> Edges { get; set; }
-
+        /// <summary>
+        /// point to the node which was created with "this node" in one step
+        /// </summary>
         public Node Link { get; set; }
-
+        /// <summary>
+        /// init the node attributes
+        /// </summary>
         public Node()
         {
             Edges = new Dictionary<char, Edge>();

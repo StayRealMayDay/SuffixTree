bb644342fd0604c3ad859d513646b51477e14b8d
renhaoran <RHRxjtu@outlook.com>
2018-05-07 13:00:27 +0800

    changge the kMeas Algo implement

--- KMeans.cs
+++ KMeans.cs
@@ -9,66 +9,180 @@ namespace SuffixTree
         {
             Data = data;
             K = k;
-            CategoryDic = new Dictionary<int, List<Tuple<double, double>>>();
+            ModelList = new List<char>();
+//            CategoryDic = new Dictionary<int, List<Tuple<double, double>>>();
+//            OldCategoryDic = new Dictionary<int, List<Tuple<double, double>>>();
         }
 
-        public List<Tuple<double, double>> KMeansAlgro()
+//        public List<Tuple<double, double>> KMeansAlgro()
+//        {
+//            var CategoryChanged = true;
+//            var KMiddlePoint = new List<Tuple<double, double>>();
+//            for (int i = 0; i < K; i++)
+//            {
+//                KMiddlePoint.Add(Data[i]);
+//            }
+//
+//            while (CategoryChanged)
+//            {
+//                CategoryDic.Clear();
+//                CategoryChanged = false;
+//                foreach (var tuple in Data)
+//                {
+//                    var category = 0;
+//                    var minDistance = Math.Pow(tuple.Item1 - KMiddlePoint[0].Item1, 2) +
+//                                      Math.Pow(tuple.Item2 - KMiddlePoint[0].Item2, 2);
+//                    for (int i = 1; i < K; i++)
+//                    {
+//                        var temp = Math.Pow(tuple.Item1 - KMiddlePoint[i].Item1, 2) +
+//                                   Math.Pow(tuple.Item2 - KMiddlePoint[i].Item2, 2);
+//                        if (temp < minDistance)
+//                        {
+//                            category = i;
+//                            minDistance = temp;
+//                        }
+//                    }
+//
+//                    if (OldCategoryDic.ContainsKey(category) && !OldCategoryDic[category].Contains(tuple))
+//                    {
+//                        CategoryChanged = true;
+//                    }
+//
+//                    if (!CategoryDic.ContainsKey(category))
+//                    {
+//                        CategoryDic.Add(category, new List<Tuple<double, double>>());
+//                    }
+//
+//                    CategoryDic[category].Add(tuple);
+//                }
+//
+//                OldCategoryDic = new Dictionary<int, List<Tuple<double, double>>>(CategoryDic);
+//                foreach (var keyValue in CategoryDic)
+//                {
+//                    var item1 = 0.0;
+//                    var item2 = 0.0;
+//                    foreach (var tuple in keyValue.Value)
+//                    {
+//                        item1 += tuple.Item1;
+//                        item2 += tuple.Item2;
+//                    }
+//
+//                    KMiddlePoint[keyValue.Key] =
+//                        new Tuple<double, double>(item1 / keyValue.Value.Count, item2 / keyValue.Value.Count);
+//                }
+//            }
+//
+//            return KMiddlePoint;
+//        }
+//
+//        private Dictionary<int, List<Tuple<double, double>>> OldCategoryDic { get; set; }
+//
+//        private Dictionary<int, List<Tuple<double, double>>> CategoryDic { get; set; }
+
+        public List<char> KMeansAlg()
         {
-            var CategoryChanged = true;
-            var KMiddlePoint = new List<Tuple<double, double>>();
+            Category = new int[Data.Count];
+            var categoryChanged = true;
+            var kMiddlePoint = new List<Tuple<double, double>>();
             for (int i = 0; i < K; i++)
             {
-                KMiddlePoint.Add(Data[i]);
+                kMiddlePoint.Add(Data[i]);
+                Category[i] = i;
             }
-            
-            while (CategoryChanged)
+
+            while (categoryChanged)
             {
-                OldCategoryDic = new Dictionary<int, List<Tuple<double, double>>>(CategoryDic);
-                CategoryDic.Clear();
-                CategoryChanged = false;
-                foreach (var tuple in Data)
+                categoryChanged = false;
+                for (int i = 0; i < Data.Count; i++)
                 {
                     var category = 0;
-                    var minDistance = Math.Pow(tuple.Item1 - KMiddlePoint[0].Item1, 2) +
-                                      Math.Pow(tuple.Item2 - KMiddlePoint[0].Item2, 2);
-                    for (int i = 1; i < K; i++)
+                    var minDistance = Math.Pow(Data[i].Item1 - kMiddlePoint[0].Item1, 2) +
+                                      Math.Pow(Data[i].Item2 - kMiddlePoint[0].Item2, 2);
+                    for (int j = 1; j < K; j++)
                     {
-                        var temp = Math.Pow(tuple.Item1 - KMiddlePoint[i].Item1, 2) +
-                                   Math.Pow(tuple.Item2 - KMiddlePoint[i].Item2, 2);
+                        var temp = Math.Pow(Data[i].Item1 - kMiddlePoint[j].Item1, 2) +
+                                   Math.Pow(Data[i].Item2 - kMiddlePoint[j].Item2, 2);
                         if (temp < minDistance)
                         {
-                            category = i;
+                            category = j;
                             minDistance = temp;
                         }
                     }
 
-                    if (!CategoryDic.ContainsKey(category))
+                    if (Category[i] != category)
                     {
-                        CategoryDic.Add(category, new List<Tuple<double, double>>());
+                        categoryChanged = true;
+                        Category[i] = category;
                     }
-                    CategoryDic[category].Add(tuple);
-                }
 
-                foreach (var keyValue in CategoryDic)
-                {
-                    var item1 = 0.0;
-                    var item2 = 0.0;
-                    foreach (var tuple in keyValue.Value)
+                    var KCount = new int[K];
+                    var KArray = new Double[K, 2];
+                    for (int j = 0; j < Data.Count; j++)
                     {
-                        item1 += tuple.Item1;
-                        item2 += tuple.Item2;
+                        KCount[Category[j]]++;
+                        KArray[Category[j], 0] += Data[j].Item1;
+                        KArray[Category[j], 1] += Data[j].Item2;
                     }
-                    KMiddlePoint[keyValue.Key] = new Tuple<double, double>(item1 / keyValue.Value.Count, item2 / keyValue.Value.Count);
+
+                    for (int j = 0; j < K; j++)
+                    {
+                        kMiddlePoint[j] = new Tuple<double, double>(KArray[j, 0] / KCount[j], KArray[j, 1] / KCount[j]);
+                    }
                 }
             }
-            return KMiddlePoint;
+
+            for (int i = 0; i < Category.Length; i++)
+            {
+                var temp = '0';
+                if (Category[i] == 1)
+                {
+                    temp = '1';
+                }
+
+                if (Category[i] == 2)
+                {
+                    temp = '2';
+                }
+
+                if (Category[i] == 3)
+                {
+                    temp = '3';
+                }
+                if (Category[i] == 4)
+                {
+                    temp = '4';
+                }
+
+                if (Category[i] == 5)
+                {
+                    temp = '5';
+                }
+
+                if (Category[i] == 6)
+                {
+                    temp = '6';
+                }
+                if (Category[i] == 7)
+                {
+                    temp = '7';
+                }
+
+                if (Category[i] == 8)
+                {
+                    temp = '8';
+                }
+
+                ModelList.Add(temp);
+            }
+
+            return ModelList;
         }
 
-        private Dictionary<int, List<Tuple<double, double>>> OldCategoryDic { get; set; }
-        
-        private Dictionary<int, List<Tuple<double, double>>> CategoryDic { get; set; }
-
         private int K { get; set; }
+
+        private int[] Category { get; set; }
+
+        private List<char> ModelList { get; set; }
 
         private List<Tuple<double, double>> Data { get; set; }
     }
